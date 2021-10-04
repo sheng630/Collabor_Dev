669917a7d567c527688220f28947ca4620979c11
Hanqi Sheng <hanqi.sheng.ca@gmail.com>
2021-10-03 20:05:49 -0400

    modified num1 value

--- login.py
+++ login.py
@@ -1,10 +1,6 @@
 
-num1 = 10 #Code from ClientA
+num1 = 1 #Code from ClientA
 num4 = 40 #Code from ClientB
 num2 = 10 #Code from ClientB
-
-<<<<<<< HEAD
-
-=======
 num3 = 30 #Code from ClientA
->>>>>>> 01128cc412bfff7a5d17d34b5ba87564bd244632
+
